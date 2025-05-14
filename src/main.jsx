77882123741a0250cc1ed194f1fb59987fddd76f
Guadalupe Gonçalves <guadalupe.goncalves@gmail.com>
2025-05-13 23:33:07 -0300

    repo nuevo con correcciones para render

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import OrderProvider from './context/OrderContext.jsx'
-import UserProvider from './context/UserContext.jsx'
+import OrderProvider from './Context/OrderContext.jsx'
+import UserProvider from './Context/UserContext.jsx'
 
 createRoot(document.getElementById('root')).render(
 
